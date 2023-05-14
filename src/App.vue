cf04246ed9589b681f7edda9b8988354e8d9a3cd
Giacomo Salici <giambo2000@gmail.com>
2023-05-14 13:19:15 +0200

    ui well defined

--- src/App.vue
+++ src/App.vue
@@ -9,47 +9,47 @@ var istant_query = ref({
   time: "0",
   data: {
     outdoor: {
-      temperature: { time: " ", unit: " ",     value: "0" },
-      feels_like: { time: " ", unit: " ",     value: "0" },
-      app_temp: { time: " ", unit: " ",     value: "0" },
-      dew_point: { time: " ", unit: " ",     value: "0" },
-      humidity: { time: " ", unit: " ",   value: "0" },
+      temperature: { time: " ", unit: " ", value: "0" },
+      feels_like: { time: " ", unit: " ", value: "0" },
+      app_temp: { time: " ", unit: " ", value: "0" },
+      dew_point: { time: " ", unit: " ", value: "0" },
+      humidity: { time: " ", unit: " ", value: "0" },
     },
     indoor: {
-      temperature: { time: " ", unit: " ",     value: "0" },
-      humidity: { time: " ", unit: " ",   value: "0" },
+      temperature: { time: " ", unit: " ", value: "0" },
+      humidity: { time: " ", unit: " ", value: "0" },
     },
     solar_and_uvi: {
-      solar: { time: " ", unit: " ",     value: "0" },
-      uvi: { time: " ", unit: "",   value: "0" },
+      solar: { time: " ", unit: " ", value: "0" },
+      uvi: { time: " ", unit: "", value: "0" },
     },
     rainfall: {
-      rain_rate: { time: " ", unit: " ",     value: "0" },
-      daily: { time: " ", unit: " ",     value: "0" },
-      event: { time: " ", unit: " ",     value: "0" },
-      hourly: { time: " ", unit: " ",     value: "0" },
-      weekly: { time: " ", unit: " ",     value: "0" },
-      monthly: { time: " ", unit: " ",     value: "0" },
-      yearly: { time: " ", unit: " ",     value: "0" },
+      rain_rate: { time: " ", unit: " ", value: "0" },
+      daily: { time: " ", unit: " ", value: "0" },
+      event: { time: " ", unit: " ", value: "0" },
+      hourly: { time: " ", unit: " ", value: "0" },
+      weekly: { time: " ", unit: " ", value: "0" },
+      monthly: { time: " ", unit: " ", value: "0" },
+      yearly: { time: " ", unit: " ", value: "0" },
     },
     wind: {
-      wind_speed: { time: " ", unit: " ",     value: "0" },
-      wind_gust: { time: " ", unit: " ",     value: "0" },
-      wind_direction: { time: " ", unit: " ",     value: "0" },
+      wind_speed: { time: " ", unit: " ", value: "0" },
+      wind_gust: { time: " ", unit: " ", value: "0" },
+      wind_direction: { time: " ", unit: " ", value: "0" },
     },
     pressure: {
-      relative: { time: " ", unit: " ",     value: "0" },
-      absolute: { time: " ", unit: " ",     value: "0" },
+      relative: { time: " ", unit: " ", value: "0" },
+      absolute: { time: " ", unit: " ", value: "0" },
     },
-    battery: { sensor_array: { time: " ", unit: " ",   value: "0" } },
+    battery: { sensor_array: { time: " ", unit: " ", value: "0" } },
   },
 });
 
 watchEffect(async () => {
   const res = await fetch(api_url);
-  istant_query = await res.json();
+  istant_query.value = await res.json();
   console.log(istant_query);
-});
+}).bind();
 </script>
 
 <template>
@@ -76,9 +76,13 @@ watchEffect(async () => {
     </div>
   </div>
 
+  
+  <h1 class="text-2xl font-bold	m-2 mb-4 bg-base-100 shadow-xl p-5 rounded-xl">Weather Data <span class=" badge badge-outline badge-info">updated</span></h1>
+
+
   <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 m-2">
     <!--RAIN-->
-    <div class="stats stats-vertical md:stats-horizzontal shadow-xl">
+    <div class="stats stats-vertical md:stats-horizontal shadow-xl">
       <div class="stat">
         <div class="stat-figure text-secondary">
           <div class="w-16">
@@ -90,12 +94,15 @@ watchEffect(async () => {
         </div>
         <div class="stat-title">Current rainfall rate</div>
         <div class="stat-value">
-          {{ istant_query.data.rainfall.rain_rate.value}}
-          <span class="text-sm">{{ istant_query.data.rainfall.rain_rate.unit}}</span>
+          {{ istant_query.data.rainfall.rain_rate.value }}
+          <span class="text-sm">{{
+            istant_query.data.rainfall.rain_rate.unit
+          }}</span>
         </div>
         <div class="stat-desc">
           in the last 5 min. <br />
-          Hourly: {{istant_query.data.rainfall.hourly.value}} {{istant_query.data.rainfall.hourly.unit}} 
+          Hourly: {{ istant_query.data.rainfall.hourly.value }}
+          {{ istant_query.data.rainfall.hourly.unit }}
         </div>
       </div>
 
@@ -107,14 +114,24 @@ watchEffect(async () => {
         </div>
         <div class="stat-title">Total rainfall event</div>
         <div class="stat-value">
-          {{ istant_query.data.rainfall.event.value }} <span class="text-sm">{{ istant_query.data.rainfall.event.unit }}</span>
+          {{ istant_query.data.rainfall.event.value }}
+          <span class="text-sm">{{
+            istant_query.data.rainfall.event.unit
+          }}</span>
         </div>
-        <div class="stat-desc">Weekly: {{istant_query.data.rainfall.weekly.value}} {{istant_query.data.rainfall.weekly.unit}}<br />Monthly: {{istant_query.data.rainfall.monthly.value}} {{istant_query.data.rainfall.monthly.unit}}<br />Yearly: {{istant_query.data.rainfall.yearly.value}} {{istant_query.data.rainfall.yearly.unit}}</div>
+        <div class="stat-desc">
+          Weekly: {{ istant_query.data.rainfall.weekly.value }}
+          {{ istant_query.data.rainfall.weekly.unit }}<br />Monthly:
+          {{ istant_query.data.rainfall.monthly.value }}
+          {{ istant_query.data.rainfall.monthly.unit }}<br />Yearly:
+          {{ istant_query.data.rainfall.yearly.value }}
+          {{ istant_query.data.rainfall.yearly.unit }}
+        </div>
       </div>
     </div>
 
     <!--OUTDOOR STATS-->
-    <div class="stats stats-vertical md:stats-horizzontal shadow-xl">
+    <div class="stats stats-vertical md:stats-horizontal shadow-xl">
       <div class="stat">
         <div class="stat-figure text-secondary">
           <div class="w-16">
@@ -125,55 +142,171 @@ watchEffect(async () => {
           </div>
         </div>
         <div class="stat-title">Outdoor temperature</div>
-        <div class="stat-value">{{istant_query.data.outdoor.temperature.value}} <span class="text-sm">{{istant_query.data.outdoor.temperature.unit}}</span></div>
+        <div class="stat-value">
+          {{ istant_query.data.outdoor.temperature.value }}
+          <span class="text-sm">{{
+            istant_query.data.outdoor.temperature.unit
+          }}</span>
+        </div>
         <div class="stat-desc">
-         
+          Dew point: {{ istant_query.data.outdoor.dew_point.value
+          }}{{ istant_query.data.outdoor.dew_point.unit }}
         </div>
       </div>
 
       <div class="stat">
         <div class="stat-figure text-secondary">
           <div class="w-16">
-            <img src="https://img.icons8.com/fluency/wet.svg" alt="wet" />
+            <img src="https://img.icons8.com/fluency/barometer-gauge.svg" alt="barometer-gauge" />
           </div>
         </div>
         <div class="stat-title">Outdoor humidity</div>
-        <div class="stat-value">{{istant_query.data.outdoor.humidity.value}} <span class="text-sm">{{istant_query.data.outdoor.humidity.unit}}</span></div>
+        <div class="stat-value">
+          {{ istant_query.data.outdoor.humidity.value }}
+          <span class="text-sm">{{
+            istant_query.data.outdoor.humidity.unit
+          }}</span>
+        </div>
         <div class="stat-desc">
+          Abs. pressure: {{ istant_query.data.pressure.absolute.value }}
+          {{ istant_query.data.pressure.absolute.unit }}
         </div>
       </div>
     </div>
 
-     <!--INDOOR STATS-->
-     <div class="stats stats-vertical md:stats-horizzontal shadow-xl">
+
+    <!--SOLAR AND UVI-->
+    <div class="stats stats-vertical md:stats-horizontal shadow-xl">
+      <div class="stat">
+        <div class="stat-figure text-secondary">
+          <div class="w-16">
+            <img
+              src="https://img.icons8.com/fluency/brightness-settings.svg"
+              alt="brightness"
+            />
+          </div>
+        </div>
+        <div class="stat-title">Solar Irradiance</div>
+        <div class="stat-value">
+          {{ istant_query.data.solar_and_uvi.solar.value }}
+          <span class="text-sm">{{
+            istant_query.data.solar_and_uvi.solar.unit
+          }}</span>
+        </div>
+        <div class="stat-desc"></div>
+      </div>
+
+      <div class="stat">
+        <div class="stat-figure text-secondary">
+          <div class="w-16">
+            <img
+              src="https://img.icons8.com/fluency/solar-energy.svg"
+              alt="solar-energy"
+            />
+          </div>
+        </div>
+        <div class="stat-title">UV Index</div>
+        <div class="stat-value">
+          {{ istant_query.data.solar_and_uvi.uvi.value }}
+          <span class="text-sm">{{
+            istant_query.data.solar_and_uvi.uvi.unit
+          }}</span>
+        </div>
+        <div class="stat-desc"></div>
+      </div>
+    </div>
+
+    <!--WIND-->
+    <div class="stats stats-vertical md:stats-horizontal shadow-xl">
+      <div class="stat">
+        <div class="stat-figure text-secondary">
+          <div class="w-16">
+            <img
+              src="https://img.icons8.com/fluency/windsock.svg"
+              alt="windsock"
+            />
+          </div>
+        </div>
+        <div class="stat-title">Wind Speed</div>
+        <div class="stat-value">
+          {{ istant_query.data.wind.wind_speed.value }}
+          <span class="text-sm">{{
+            istant_query.data.wind.wind_speed.unit
+          }}</span>
+        </div>
+        <div class="stat-desc">Gust: {{istant_query.data.wind.wind_gust.value}}{{istant_query.data.wind.wind_gust.unit}}</div>
+      </div>
+
+      <div class="stat">
+        <div class="stat-figure text-secondary">
+          <div class="w-16">
+            <img
+              src="https://img.icons8.com/fluency/wind-rose.svg"
+              alt="windrose"
+            />
+          </div>
+        </div>
+        <div class="stat-title">Wind Direction</div>
+        <div class="stat-value">
+          {{ istant_query.data.wind.wind_direction.value }}
+          <span class="text-sm">{{
+            istant_query.data.wind.wind_direction.unit
+          }}</span>
+        </div>
+        <div class="stat-desc"></div>
+      </div>
+    </div>
+  </div>
+
+  <h1 class="text-2xl font-bold	m-2 mb-4 mt-6 bg-base-100 shadow-xl p-5 rounded-xl">Other Stats <span class=" badge badge-outline badge-info">updated</span></h1>
+
+
+  <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 m-2">
+
+    <!--INDOOR STATS-->
+    <div class="stats stats-vertical md:stats-horizontal shadow-xl">
       <div class="stat">
         <div class="stat-figure text-secondary">
           <div class="w-16">
             <img
               src="https://img.icons8.com/fluency/temperature-inside.svg"
-              alt="temperature-outside"
+              alt="temperature-inside"
             />
           </div>
         </div>
         <div class="stat-title">Indoor temperature</div>
-        <div class="stat-value">{{istant_query.data.indoor.temperature.value}} <span class="text-sm">{{istant_query.data.indoor.temperature.unit}}</span></div>
-        <div class="stat-desc">
-         
+        <div class="stat-value">
+          {{ istant_query.data.indoor.temperature.value }}
+          <span class="text-sm">{{
+            istant_query.data.indoor.temperature.unit
+          }}</span>
         </div>
-      </div>
+        <div class="stat-desc">Indoor humidity: {{ istant_query.data.indoor.humidity.value }}{{
+            istant_query.data.indoor.humidity.unit
+          }}</div>
 
+      
+      </div>
       <div class="stat">
         <div class="stat-figure text-secondary">
           <div class="w-16">
-            <img src="https://img.icons8.com/fluency/humidity.svg" alt="humidity" />
+            <img
+              src="https://img.icons8.com/fluency/medium-battery.svg"
+              alt="battery"
+            />
           </div>
         </div>
-        <div class="stat-title">Indoor humidity</div>
-        <div class="stat-value">{{istant_query.data.indoor.humidity.value}} <span class="text-sm">{{istant_query.data.indoor.humidity.unit}}</span></div>
-        <div class="stat-desc">
+        <div class="stat-title">Battery Level</div>
+        <div class="stat-value">
+          
+            
+          <span v-if="istant_query.data.battery.sensor_array.value == '0'">Normal
+          </span><span v-else>Low</span>
+          
         </div>
+        
       </div>
+     
     </div>
   </div>
-  
 </template>
