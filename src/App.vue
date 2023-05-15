36a5c5cd5d587acad6206c1f133fc103f3ec4709
Giacomo Salici <giambo2000@gmail.com>
2023-05-15 12:40:47 +0200

    statcomponent

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { onMounted, onUnmounted, watch, ref, watchEffect } from "vue";
-import moment from "moment"
+import moment from "moment";
+import StatComponent from "./components/StatComponent.vue";
 
 const api_url = `https://weather-station-api.jacksalici.workers.dev/`;
 
@@ -46,32 +47,32 @@ var istant_query = ref({
   },
 });
 
-var loading = ref({status: true, text:"Loading..."});
+var loading = ref({ status: true, text: "Loading..." });
 
-var showMoreData = ref(false)
+var showMoreData = ref(false);
 
-async function updateData(){
-  loading.value = {status: true, text:"Loading..."};
+async function updateData() {
+  loading.value = { status: true, text: "Loading..." };
   const res = await fetch(api_url);
   istant_query.value = await res.json();
   console.log(istant_query);
-  loading.value = {status: false, text:`Updated at ${moment(istant_query.value.time * 1000).format('HH:mm')}`};
+  loading.value = {
+    status: false,
+    text: `Updated at ${moment(istant_query.value.time * 1000).format(
+      "HH:mm"
+    )}`,
+  };
 }
 
-
-
-
+const batteryLevels = ["Normal", "Low"]
 
 watchEffect(async () => {
-  updateData()
-  transformData()
+  updateData();
 });
 
-
 onUnmounted(() => {
-  cancelAnimationFrame(handle)
-})
-
+  cancelAnimationFrame(handle);
+});
 </script>
 
 <template>
@@ -94,19 +95,32 @@ onUnmounted(() => {
             stroke-width="2"
             d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
           ></path>
-        </svg>      </label>
-      <ul tabindex="0" class="menu menu-compact dropdown-content  p-2 shadow-xl bg-base-100 rounded-box w-36">
-        <li><label class="flex-1" for="show-more-data-modal"><img
+        </svg>
+      </label>
+      <ul
+        tabindex="0"
+        class="menu menu-compact dropdown-content p-2 shadow-xl bg-base-100 rounded-box w-36"
+      >
+        <li>
+          <label class="flex-1" for="show-more-data-modal"
+            ><img
               class="h-8"
               src="https://img.icons8.com/fluency/administrative-tools.svg"
               alt="settings"
-            /> Settings</label></li>
-        <li><label class="flex-1" for="show-about-modal"><img
+            />
+            Settings</label
+          >
+        </li>
+        <li>
+          <label class="flex-1" for="show-about-modal"
+            ><img
               class="h-8"
               src="https://img.icons8.com/fluency/about.svg"
               alt="about"
-            /> About</label></li>
-            
+            />
+            About</label
+          >
+        </li>
       </ul>
     </div>
   </div>
@@ -114,279 +128,193 @@ onUnmounted(() => {
   <!--NAV BAR-->
   <h1 class="text-2xl font-bold m-2 mb-4 bg-base-100 shadow-xl p-5 rounded-xl">
     Istant Data
-    <span class="btn btn-xs btn-ghost no-animation" @click="updateData" :class="{ loading : loading.status }"
-      >{{loading.text}}</span
+    <span
+      class="btn btn-xs btn-ghost no-animation"
+      :class="{ loading: loading.status }"
+      >{{ loading.text }}</span
     >
   </h1>
 
   <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 m-2">
     <!--RAIN-->
     <div class="stats stats-vertical md:stats-horizontal shadow-xl">
-      <div class="stat overflow-hidden">
-        <div class="stat-figure text-secondary">
-          <div class="w-16">
-            <img
-              src="https://img.icons8.com/fluency/hygrometer.svg"
-              alt="hygrometer"
-            />
-          </div>
-        </div>
-        <div class="stat-title">Current rainfall rate</div>
-        <div class="stat-value">
-          {{ istant_query.data.rainfall.rain_rate.value }}
-          <span class="text-sm">{{
-            istant_query.data.rainfall.rain_rate.unit
-          }}</span>
-        </div>
-        <div class="stat-desc">
-          in the last 5 min. <br />
-          Hourly: {{ istant_query.data.rainfall.hourly.value }}
-          {{ istant_query.data.rainfall.hourly.unit }}
-        </div>
-        <div class="stat-actions col-span-2 -m-10 mt-5">
-          <img src="https://images.unsplash.com/photo-1683085809775-d9ac53fcbe21?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2372&q=80"/>
-        </div>
+      <StatComponent
+        title="Current rainfall rate"
+        icon="hygrometer"
+        :value="istant_query.data.rainfall.rain_rate.value"
+        :unit="istant_query.data.rainfall.rain_rate.unit"
+        :description="
+          'in the last 5 min. <br /> Hourly: ' +
+          istant_query.data.rainfall.hourly.value +
+          istant_query.data.rainfall.hourly.unit + '<br /> Daily: ' +
+          istant_query.data.rainfall.daily.value +
+          istant_query.data.rainfall.daily.unit
+        "
+      />
 
-      </div>
-
-      <div class="stat">
-        <div class="stat-figure text-secondary">
-          <div class="w-16">
-            <img src="https://img.icons8.com/fluency/rain.svg" alt="rain" />
-          </div>
-        </div>
-        <div class="stat-title">Total rainfall event</div>
-        <div class="stat-value">
-          {{ istant_query.data.rainfall.event.value }}
-          <span class="text-sm">{{
-            istant_query.data.rainfall.event.unit
-          }}</span>
-        </div>
-        <div class="stat-desc">
-          Weekly: {{ istant_query.data.rainfall.weekly.value }}
-          {{ istant_query.data.rainfall.weekly.unit }}<br />Monthly:
-          {{ istant_query.data.rainfall.monthly.value }}
-          {{ istant_query.data.rainfall.monthly.unit }}<br />Yearly:
-          {{ istant_query.data.rainfall.yearly.value }}
-          {{ istant_query.data.rainfall.yearly.unit }}
-        </div>
-      </div>
+      <StatComponent
+        title="Total rainfall event"
+        icon="rain"
+        :value="istant_query.data.rainfall.event.value"
+        :unit="istant_query.data.rainfall.event.unit"
+        :description="
+          'Weekly:' + istant_query.data.rainfall.weekly.value +
+          istant_query.data.rainfall.weekly.unit + '<br />Monthly:' +
+          istant_query.data.rainfall.monthly.value +
+          istant_query.data.rainfall.monthly.unit + '<br />Yearly:' +
+          istant_query.data.rainfall.yearly.value +
+          istant_query.data.rainfall.yearly.unit
+        "
+      />
     </div>
 
     <!--OUTDOOR STATS-->
     <div class="stats stats-vertical md:stats-horizontal shadow-xl">
-      <div class="stat">
-        <div class="stat-figure text-secondary">
-          <div class="w-16">
-            <img
-              src="https://img.icons8.com/fluency/temperature-outside.svg"
-              alt="temperature-outside"
-            />
-          </div>
-        </div>
-        <div class="stat-title">Outdoor temperature</div>
-        <div class="stat-value">
-          {{ istant_query.data.outdoor.temperature.value }}
-          <span class="text-sm">{{
-            istant_query.data.outdoor.temperature.unit
-          }}</span>
-        </div>
-        <div class="stat-desc">
-          Dew point: {{ istant_query.data.outdoor.dew_point.value
-          }}{{ istant_query.data.outdoor.dew_point.unit }}
-        </div>
-      </div>
-
-      <div class="stat">
-        <div class="stat-figure text-secondary">
-          <div class="w-16">
-            <img
-              src="https://img.icons8.com/fluency/barometer-gauge.svg"
-              alt="barometer-gauge"
-            />
-          </div>
-        </div>
-        <div class="stat-title">Outdoor humidity</div>
-        <div class="stat-value">
-          {{ istant_query.data.outdoor.humidity.value }}
-          <span class="text-sm">{{
-            istant_query.data.outdoor.humidity.unit
-          }}</span>
-        </div>
-        <div class="stat-desc">
-          Abs. pressure: {{ istant_query.data.pressure.absolute.value }}
-          {{ istant_query.data.pressure.absolute.unit }}
-        </div>
-      </div>
+      <StatComponent
+        title="Outdoor temperature"
+        icon="temperature-outside"
+        :value="istant_query.data.outdoor.temperature.value"
+        :unit="istant_query.data.outdoor.temperature.unit"
+        :description="
+          'Dew point: ' +
+          istant_query.data.outdoor.dew_point.value +
+          istant_query.data.outdoor.dew_point.unit 
+        "
+      />
+      
+      <StatComponent
+        title="Outdoor humidity"
+        icon="barometer-gauge"
+        :value="istant_query.data.outdoor.humidity.value"
+        :unit="istant_query.data.outdoor.humidity.unit"
+        :description="
+          'Abs. Pressure: ' +
+          istant_query.data.pressure.absolute.value +
+          istant_query.data.pressure.absolute.unit 
+        "
+      />
+    
     </div>
 
     <!--SOLAR AND UVI-->
     <div class="stats stats-vertical md:stats-horizontal shadow-xl">
-      <div class="stat">
-        <div class="stat-figure text-secondary">
-          <div class="w-16">
-            <img
-              src="https://img.icons8.com/fluency/brightness-settings.svg"
-              alt="brightness"
-            />
-          </div>
-        </div>
-        <div class="stat-title">Solar Irradiance</div>
-        <div class="stat-value">
-          {{ istant_query.data.solar_and_uvi.solar.value }}
-          <span class="text-sm">{{
-            istant_query.data.solar_and_uvi.solar.unit
-          }}</span>
-        </div>
-        <div class="stat-desc"></div>
-      </div>
+      
+      <StatComponent
+        title="Solar Irradiance"
+        icon="brightness-settings"
+        :value="istant_query.data.solar_and_uvi.solar.value"
+        :unit="istant_query.data.solar_and_uvi.solar.unit"
+      />
+      <StatComponent
+        title="UV Index"
+        icon="solar-energy"
+        :value="istant_query.data.solar_and_uvi.uvi.value"
+        :unit="istant_query.data.solar_and_uvi.uvi.unit"
+      />
 
-      <div class="stat">
-        <div class="stat-figure text-secondary">
-          <div class="w-16">
-            <img
-              src="https://img.icons8.com/fluency/solar-energy.svg"
-              alt="solar-energy"
-            />
-          </div>
-        </div>
-        <div class="stat-title">UV Index</div>
-        <div class="stat-value">
-          {{ istant_query.data.solar_and_uvi.uvi.value }}
-          <span class="text-sm">{{
-            istant_query.data.solar_and_uvi.uvi.unit
-          }}</span>
-        </div>
-        <div class="stat-desc"></div>
-      </div>
     </div>
 
     <!--WIND-->
     <div class="stats stats-vertical md:stats-horizontal shadow-xl">
-      <div class="stat">
-        <div class="stat-figure text-secondary">
-          <div class="w-16">
-            <img
-              src="https://img.icons8.com/fluency/windsock.svg"
-              alt="windsock"
-            />
-          </div>
-        </div>
-        <div class="stat-title">Wind Speed</div>
-        <div class="stat-value">
-          {{ istant_query.data.wind.wind_speed.value }}
-          <span class="text-sm">{{
-            istant_query.data.wind.wind_speed.unit
-          }}</span>
-        </div>
-        <div class="stat-desc">
-          Gust: {{ istant_query.data.wind.wind_gust.value
-          }}{{ istant_query.data.wind.wind_gust.unit }}
-        </div>
-      </div>
-
-      <div class="stat">
-        <div class="stat-figure text-secondary">
-          <div class="w-16">
-            <img
-              src="https://img.icons8.com/fluency/wind-rose.svg"
-              alt="windrose"
-            />
-          </div>
-        </div>
-        <div class="stat-title">Wind Direction</div>
-        <div class="stat-value">
-          {{ istant_query.data.wind.wind_direction.value }}
-          <span class="text-sm">{{
-            istant_query.data.wind.wind_direction.unit
-          }}</span>
-        </div>
-        <div class="stat-desc"></div>
-      </div>
+      <StatComponent
+        title="Wind speed"
+        icon="windsock"
+        :value="istant_query.data.wind.wind_speed.value"
+        :unit="istant_query.data.wind.wind_speed.unit"
+        :description="
+        'Wind Gust: ' +
+        istant_query.data.wind.wind_gust.value +
+        istant_query.data.wind.wind_gust.unit
+        "
+      />
+      
+      <StatComponent
+        title="Wind speed"
+        icon="wind-rose"
+        :value="istant_query.data.wind.wind_direction.value"
+        :unit="istant_query.data.wind.wind_direction.unit"
+      />
+     
     </div>
   </div>
 
   <h1
-    class="text-2xl font-bold m-2 mb-4 mt-6 bg-base-100 shadow-xl p-5 rounded-xl"  v-if="showMoreData"
+    class="text-2xl font-bold m-2 mb-4 mt-6 bg-base-100 shadow-xl p-5 rounded-xl"
+    v-if="showMoreData"
   >
     Other Stats
-    <span class="btn btn-xs btn-ghost no-animation" @click="updateData" :class="{ loading : loading.status }"
-      >{{loading.text}}</span
+    <span
+      class="btn btn-xs btn-ghost no-animation"
+      :class="{ loading: loading.status }"
+      >{{ loading.text }}</span
     >
   </h1>
 
   <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 m-2" v-if="showMoreData">
     <!--INDOOR STATS-->
-    <div class="stats stats-vertical md:stats-horizontal shadow-xl">
-      <div class="stat">
-        <div class="stat-figure text-secondary">
-          <div class="w-16">
-            <img
-              src="https://img.icons8.com/fluency/temperature-inside.svg"
-              alt="temperature-inside"
-            />
-          </div>
-        </div>
-        <div class="stat-title">Indoor temperature</div>
-        <div class="stat-value">
-          {{ istant_query.data.indoor.temperature.value }}
-          <span class="text-sm">{{
-            istant_query.data.indoor.temperature.unit
-          }}</span>
-        </div>
-        <div class="stat-desc">
-          Indoor humidity: {{ istant_query.data.indoor.humidity.value
-          }}{{ istant_query.data.indoor.humidity.unit }}
-        </div>
-      </div>
-      <div class="stat">
-        <div class="stat-figure text-secondary">
-          <div class="w-16">
-            <img
-              src="https://img.icons8.com/fluency/medium-battery.svg"
-              alt="battery"
-            />
-          </div>
-        </div>
-        <div class="stat-title">Battery Level</div>
-        <div class="stat-value">
-          <span v-if="istant_query.data.battery.sensor_array.value == '0'"
-            >Normal </span
-          ><span v-else>Low</span>
-        </div>
-        
-      </div>
-    </div>
+    <div class="stats stats-vertical md:stats-horizontal shadow-xl" v-if="showMoreData">
+      <StatComponent
+        title="Inside temperature"
+        icon="temperature-inside"
+        :value="istant_query.data.indoor.temperature.value"
+        :unit="istant_query.data.indoor.temperature.unit"
+        :description="
+        'Indoor humidity: ' +
+        istant_query.data.indoor.humidity.value +
+        istant_query.data.indoor.humidity.unit
+        "
+      />
+      
+      <StatComponent
+        title="Battery Level"
+        icon="battery"
+        :value="batteryLevels[parseInt(istant_query.data.battery.sensor_array.value)]"
+      />
   </div>
-
+  </div>
 
   <footer class="footer footer-center p-4 bg-base-100 text-base-content">
-  <div class="text-base-300">
-    <p>Developed and designed by <a class="link font-bold" href="https://jacksalici.com">jacksalici</a><br/>MIT Licence {{ moment().format('YYYY') }} - Icons by <a class="link" href="https://icons8.com/">Icons8</a></p>
-  </div>
-</footer>
+    <div class="text-base-300">
+      <p>
+        Developed and designed by
+        <a class="link font-bold" href="https://jacksalici.com">jacksalici</a
+        ><br />MIT Licence {{ moment().format("YYYY") }} - Icons by
+        <a class="link" href="https://icons8.com/">Icons8</a>
+      </p>
+    </div>
+  </footer>
 
-<input type="checkbox" id="show-more-data-modal" class="modal-toggle" />
-<label for="show-more-data-modal" class="modal cursor-pointer modal-bottom sm:modal-middle">
-  <label class="modal-box relative">
-    <h3 class="font-bold text-lg">Sort of ettings</h3>
-    
-    <div class="form-control">
-  <label class="label cursor-pointer">
-    <span class="label-text">Show more data</span> 
-    <input type="checkbox" class="toggle" v-model="showMoreData"/>
-  </label>
-  </div>
-  </label>
-</label>
+  <input type="checkbox" id="show-more-data-modal" class="modal-toggle" />
+  <label
+    for="show-more-data-modal"
+    class="modal cursor-pointer modal-bottom sm:modal-middle"
+  >
+    <label class="modal-box relative">
+      <h3 class="font-bold text-lg">Sort of settings</h3>
 
-<input type="checkbox" id="show-about-modal" class="modal-toggle" />
-<label for="show-about-modal" class="modal cursor-pointer modal-bottom sm:modal-middle">
-  <label class="modal-box relative">
-    <h3 class="font-bold text-lg">About</h3>
-    <p class="font-sm">I didn't have any clue what to write here. If you are a developer check the <a class="link" href="https://github.com/jacksalici/weather_station">project repo</a>.</p>
-    
+      <div class="form-control">
+        <label class="label cursor-pointer">
+          <span class="label-text">Show more data</span>
+          <input type="checkbox" class="toggle" v-model="showMoreData" />
+        </label>
+      </div>
+    </label>
   </label>
-</label>
 
+  <input type="checkbox" id="show-about-modal" class="modal-toggle" />
+  <label
+    for="show-about-modal"
+    class="modal cursor-pointer modal-bottom sm:modal-middle"
+  >
+    <label class="modal-box relative">
+      <h3 class="font-bold text-lg">About</h3>
+      <p class="font-sm">
+        I didn't have any clue what to write here. If you are a developer check
+        the
+        <a class="link" href="https://github.com/jacksalici/weather_station"
+          >project repo</a
+        >.
+      </p>
+    </label>
+  </label>
 </template>
---
+++ src/components/StatComponent.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps(["icon", "title", "value", "unit", "description"]);
+</script>
+
+<template>
+  <div class="stat overflow-hidden">
+    <div class="stat-figure text-secondary">
+      <div class="w-16">
+        <img :src="'https://img.icons8.com/fluency/'+icon+'.svg'" alt="{{icon}}" />
+      </div>
+    </div>
+    <div class="stat-title">{{ title }}</div>
+    <div class="stat-value">
+      {{ value }}
+      <span class="text-sm">{{ unit }}</span>
+    </div>
+    <div class="stat-desc" v-html="description"></div>
+    <!--<div class="stat-actions col-span-2 -m-10 mt-5">
+          <img src="https://images.unsplash.com/photo-1683085809775-d9ac53fcbe21?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2372&q=80"/>
+        </div>-->
+  </div>
+</template>
