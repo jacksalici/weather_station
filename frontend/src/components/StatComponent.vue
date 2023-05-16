ccf1943f8efecf27f5dbb521fe38be93a7011a5a
Giacomo Salici <giambo2000@gmail.com>
2023-05-16 15:13:37 +0200

    chart init

--- frontend/src/components/StatComponent.vue
+++ frontend/src/components/StatComponent.vue
@@ -1,12 +1,62 @@
 <script setup>
-defineProps(["icon", "title", "value", "unit", "description"]);
+import { Line } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  LineElement,
+  CategoryScale,
+  LinearScale
+);
+
+const props = defineProps({
+  icon: String,
+  title: String,
+  value: String,
+  unit: String,
+  description: String,
+  chart: {
+    type: Object,
+    default: {}
+  },
+});
+
+console.log(props.chart);
+let a =  Object.keys(props.chart)
+console.log(a)
+const chartOptions = {
+  responsive: true,
+};
+
+const chartData = {
+  labels:a,
+  datasets: [
+    {
+      data: Object.values(props.chart),
+    },
+  ],
+};
 </script>
 
 <template>
   <div class="stat overflow-hidden">
     <div class="stat-figure text-secondary">
       <div class="w-16">
-        <img :src="'https://img.icons8.com/fluency/'+icon+'.svg'" alt="{{icon}}" />
+        <img
+          :src="'https://img.icons8.com/fluency/' + icon + '.svg'"
+          alt="{{icon}}"
+        />
       </div>
     </div>
     <div class="stat-title">{{ title }}</div>
@@ -15,8 +65,8 @@ defineProps(["icon", "title", "value", "unit", "description"]);
       <span class="text-sm">{{ unit }}</span>
     </div>
     <div class="stat-desc" v-html="description"></div>
-    <!--<div class="stat-actions col-span-2 -m-10 mt-5">
-          <img src="https://images.unsplash.com/photo-1683085809775-d9ac53fcbe21?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2372&q=80"/>
-        </div>-->
+    <div class="stat-actions col-span-2 mt-5" v-if="Object.keys(chart).length">
+      <Line :data="chartData" :options="chartOptions" id="Line" />
+    </div>
   </div>
 </template>
